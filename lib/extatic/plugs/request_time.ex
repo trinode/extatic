cd1c87ef9d56147a97c418ca4ad65ffd26e2a68c
Anthony Graham <github@trinode.co.uk>
2016-07-26 10:29:54 +0100

    request_time_plug

--- lib/extatic/plugs/request_time.ex
+++ lib/extatic/plugs/request_time.ex
@@ -1,4 +1,4 @@
-defmodule Extatic.ExtaticPlug do
+defmodule Extatic.RequestTime do
   @behaviour Plug
   import Plug.Conn, only: [register_before_send: 2]
 
@@ -10,7 +10,7 @@ defmodule Extatic.ExtaticPlug do
     register_before_send conn, fn conn ->
       after_time = DateTime.utc_now |>  DateTime.to_unix(:microseconds)
       diff       = after_time - before_time
-      
+
       Extatic.record_timing(:response_time, diff)
 
       conn
